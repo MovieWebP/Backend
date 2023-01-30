3f45ab03cedbf265a9e206447aa1f3a85ff0726b
Justin <kidjustinchoi@gmail.com>
2023-01-30 15:25:05 +0900

    add views folder

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { VideosController } from './videos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Videos } from './entities/videos.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Videos])],
   providers: [VideosService],
-  controllers: [VideosController]
+  controllers: [VideosController],
 })
 export class VideosModule {}
